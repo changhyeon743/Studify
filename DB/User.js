8406920d1de0afbb5c8cf71d45e3647a6d7ad3be
changhyeon743 <changhyeon743@gmail.com>
2019-06-21 20:26:59 +0900

    commitg

--- DB/User.js
+++ DB/User.js
@@ -18,7 +18,7 @@ let User = new mongoose.Schema({
     average_time: Number,
     max_time: Number,
     token: String,
-    times: Number,
+    times: Number = 0,
 });
 
 let userModel = mongoose.model('users', User);
